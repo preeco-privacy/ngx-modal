d3babefa697633954e7397567d2044185c1fa291
tmiholic <t.miholic@preeco.de>
2020-02-17 12:43:03 +0100

    Updated build process to Angular package format

---
+++ src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of @preeco-privacy/ngx-modal-dialog
+ */
+
+export * from './modal-dialog.module';
+export * from './modal-dialog.service';
+export * from './modal-dialog.component';
+export * from './modal-dialog.interface';
+export * from './simple-modal.component';
