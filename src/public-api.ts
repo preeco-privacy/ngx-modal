e8f907c3be06c6aebe3eef7450f4254331a3ea55
tmiholic <t.miholic@preeco.de>
2020-02-17 13:21:01 +0100

    Export ModalDialogInstanceService

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './modal-dialog.module';
 export * from './modal-dialog.service';
+export * from './modal-dialog-instance.service';
 export * from './modal-dialog.component';
 export * from './modal-dialog.interface';
 export * from './simple-modal.component';
